371dffd75fff9ee8d3d95bf69189e13bfb190369
CarlaMassat <cm90mdp@gmail.com>
2024-06-02 19:24:34 -0300

    design and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+// import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    // SidebarComponent
   ],
   imports: [
+    GifsModule,
+    SharedModule,
     BrowserModule,
     AppRoutingModule
   ],
